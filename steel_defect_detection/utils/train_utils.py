8d75f7bd8f7055e56e48624034c2ee5cc907d9db
Joseph <jchen42703@gmail.com>
2019-09-21 07:58:53 -0400

    Initial commit

---
+++ steel_defect_detection/utils/train_utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+import random
+import torch
+
+def seed_everything(seed=42):
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed_all(SEED)
+    torch.backends.cudnn.benchmark = False  ##uses the inbuilt cudnn auto-tuner to find the fastest convolution algorithms. -
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.deterministic = True
