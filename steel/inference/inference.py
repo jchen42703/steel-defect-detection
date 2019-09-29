712a0601d3cde6a25602af5f6f88a72b639ad8ad
jchen42703 <jchen42703@gmail.com>
2019-09-29 13:33:30 -0400

    Created get_classification_predictions and load_weights_infer
    `get_classification_predictions` is just the classification version of `get_encoded_pixels` for the inevitable creation of the cascade.

--- steel/inference/inference.py
+++ steel/inference/inference.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import tqdm
+import torch
 
 from steel.io.utils import mask2rle, post_process, sigmoid
 
@@ -9,6 +10,7 @@ def get_encoded_pixels(loaders, runners, class_params):
     Processes predicted logits and converts them to encoded pixels. Does so in an iterative
     manner so operations are done image-wise rather than on the full dataset directly (to
     combat RAM limitations).
+
     Args:
         loaders: dictionary of data loaders with at least the key: "test"
         runner (an instance of a catalyst.dl.runner.SupervisedRunner):
@@ -36,3 +38,49 @@ def get_encoded_pixels(loaders, runners, class_params):
                     encoded_pixels.append(r)
                 image_id += 1
     return encoded_pixels
+
+def get_classification_predictions(loaders, runners, class_params):
+    """
+    Processes predicted logits and converts them to encoded pixels. Does so in an iterative
+    manner so operations are done image-wise rather than on the full dataset directly (to
+    combat RAM limitations).
+
+    Args:
+        loaders: dictionary of data loaders with at least the key: "test"
+        runner (an instance of a catalyst.dl.runner.SupervisedRunner):
+        class_params (dict): with keys class: threshold
+    Returns:
+        List of predictions ("" if 0 and "1" if 1)
+    """
+    predictions = []
+    image_id = 0
+    for i, test_batch in enumerate(tqdm.tqdm(loaders["test"])):
+        runner_out = runner.predict_batch({"features": test_batch[0].cuda()})["logits"]
+        # for each batch (n, h, w): resize and post_process
+        for i, batch in enumerate(runner_out):
+            for probability in batch:
+                # iterating through each probability map (h, w)
+                probability = probability.cpu().detach().numpy()
+
+                predict = cv2.threshold(sigmoid(probability), class_params[image_id % 4], 1, cv2.THRESH_BINARY)[1]
+                if predict == 0:
+                    predictions.append("")
+                else:
+                    predictions.append("1")
+                image_id += 1
+    return predictions
+
+def load_weights_infer(checkpoint_path, model):
+    """
+    Loads pytorch model from a checkpoint and into inference mode.
+
+    Args:
+        checkpoint_path (str): path to a .pt or .pth checkpoint
+        model (torch.nn.Module): <-
+    Returns:
+        Model with loaded weights and in evaluation mode
+    """
+    state_dict = torch.load(checkpoint_path, map_location="cpu")
+    model.load_state_dict(state_dict, strict=True)
+    model.eval()
+    return model
