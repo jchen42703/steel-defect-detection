76a96d522864c7100121694343fb7731287e9e8b
jchen42703 <jchen42703@gmail.com>
2019-10-07 23:04:34 -0400

    Updated Inference.get_encoded_pixels and Inference.get_classification_predictions to support applying the non-lin before resizing

--- steel/inference/inference_class.py
+++ steel/inference/inference_class.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 from functools import partial
 from torch.jit import load
-from steel.inference.utils import mask2rle, post_process, sigmoid, load_weights_infer, tta_flips_fn
+from steel.inference.utils import mask2rle, post_process, sigmoid, load_weights_infer, tta_flips_fn, apply_nonlin
 
 class Inference(object):
     def __init__(self, checkpoint_path, test_loader, test_dataset, model=None, mode="segmentation", tta_flips=None):
@@ -38,7 +38,7 @@ class Inference(object):
         if tta_flips is not None:
             assert isinstance(tta_flips, (list, tuple)), "tta_flips must be a list-like of strings."
             print(f"TTA Ops: {tta_flips}")
-            self.tta_fn = partial(tta_flips_fn, model=self.model, mode=mode, flips=tta_flips)
+            self.tta_fn = partial(tta_flips_fn, model=self.model, mode=mode, flips=tta_flips, non_lin="sigmoid")
 
     def create_sub(self, sub):
         """
@@ -78,7 +78,7 @@ class Inference(object):
             if self.tta_fn is not None:
                 pred_out = self.tta_fn(batch=test_batch[0].cuda())
             else:
-                pred_out = self.model(test_batch[0].cuda())
+                pred_out = apply_nonlin(self.model(test_batch[0].cuda()), sharpen_t=0)
             # for each batch (4, h, w): resize and post_process
             for i, batch in enumerate(pred_out):
                 for probability in batch:
@@ -86,7 +86,7 @@ class Inference(object):
                     probability = probability.cpu().detach().numpy()
                     if probability.shape != (256, 1600):
                         probability = cv2.resize(probability, dsize=(1600, 256), interpolation=cv2.INTER_LINEAR)
-                    predict, num_predict = post_process(sigmoid(probability), self.seg_class_params[image_id % 4][0],
+                    predict, num_predict = post_process(probability, self.seg_class_params[image_id % 4][0],
                                                         self.seg_class_params[image_id % 4][1])
                     if num_predict == 0:
                         encoded_pixels.append("")
@@ -110,13 +110,13 @@ class Inference(object):
             if self.tta_fn is not None:
                 pred_out = self.tta_fn(batch=test_batch[0].cuda())
             else:
-                pred_out = self.model(test_batch[0].cuda())
+                pred_out = apply_nonlin(self.model(test_batch[0].cuda()), sharpen_t=0)                
             # for each batch (n, 4): post process
             for i, batch in enumerate(pred_out):
                 # iterating through each prediction (4,)
                 probability = batch.cpu().detach().numpy()
 
-                predict = cv2.threshold(sigmoid(probability), 0.5, 1, cv2.THRESH_BINARY)[1]
+                predict = cv2.threshold(probability, 0.5, 1, cv2.THRESH_BINARY)[1]
                 # Idea: [imgid_1, imgid_2, imgid_3, imgid_4, imgid2_1,...]
                 def process(element):
                     if element == 0:
