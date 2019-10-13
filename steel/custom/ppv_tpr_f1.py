4ea28e53f2e76981e0fcb5a742a5c751668060e6
jchen42703 <jchen42703@gmail.com>
2019-10-12 22:29:35 -0400

    Added the option to specify the activation function and updated the code with the current global-metrics branch
    mainly just to keep it up to date while the PR is under review

--- steel/custom/ppv_tpr_f1.py
+++ steel/custom/ppv_tpr_f1.py
@@ -6,6 +6,7 @@ import torch
 
 from steel.custom.ppv_tpr_f1_meter import PrecisionRecallF1ScoreMeter
 from catalyst.dl.core import Callback, RunnerState, CallbackOrder
+from catalyst.utils import get_activation_fn
 
 class PrecisionRecallF1ScoreCallback(Callback):
     """
@@ -19,13 +20,28 @@ class PrecisionRecallF1ScoreCallback(Callback):
         output_key: str = "logits",
         class_names: List[str] = None,
         num_classes: int = 1,
-        threshold: float = 0.5
+        threshold: float = 0.5,
+        activation: str = "Sigmoid"
     ):
+        """
+        Args:
+            input_key (str): input key to use for iou calculation
+                specifies our ``y_true``.
+            output_key (str): output key to use for iou calculation;
+                specifies our ``y_pred``
+            class_names (List[str]): class names to display in the logs.
+                If None, defaults to indices for each class, starting from 0.
+            num_classes (int): Number of classes
+            threshold (float): threshold for outputs binarization
+            activation (str): An torch.nn activation applied to the outputs.
+                Must be one of ['none', 'Sigmoid', 'Softmax2d']
+        """
         super().__init__(CallbackOrder.Metric)
         self.input_key = input_key
         self.output_key = output_key
 
         self.list_args = ["ppv", "tpr", "f1-score"]
+        self.activation = activation
         self.class_names = class_names
         self.num_classes = num_classes \
             if class_names is None \
@@ -33,7 +49,8 @@ class PrecisionRecallF1ScoreCallback(Callback):
 
         assert self.num_classes is not None
 
-        self.meters = [PrecisionRecallF1ScoreMeter(threshold) for _ in range(self.num_classes)]
+        self.meters = [PrecisionRecallF1ScoreMeter(threshold)
+                       for _ in range(self.num_classes)]
 
     def _reset_stats(self):
         for meter in self.meters:
@@ -45,9 +62,11 @@ class PrecisionRecallF1ScoreCallback(Callback):
     def on_batch_end(self, state: RunnerState):
         logits: torch.Tensor = state.output[self.output_key].detach().float()
         targets: torch.Tensor = state.input[self.input_key].detach().float()
-        probabilities: torch.Tensor = torch.sigmoid(logits)
+        activation_fn = get_activation_fn(self.activation)
+        probabilities: torch.Tensor = activation_fn(logits)
 
         if self.num_classes == 1 and len(probabilities.shape) == 1:
+            # single meter for binary case (one class + background)
             self.meters[0].add(probabilities, targets)
         else:
             for i in range(self.num_classes):
@@ -55,22 +74,24 @@ class PrecisionRecallF1ScoreCallback(Callback):
 
     def on_loader_end(self, state: RunnerState):
         prec_recall_f1score = defaultdict(list)
+        loader_values = state.metrics.epoch_values[state.loader_name]
         for i, meter in enumerate(self.meters):
             metrics = meter.value()
             postfix = self.class_names[i] \
                 if self.class_names is not None \
                 else str(i)
             for prefix, metric_ in zip(self.list_args, metrics):
+                # adding the per-class metrics
                 prec_recall_f1score[prefix].append(metric_)
                 metric_name = f"{prefix}/class_{postfix}"
-                state.metrics.epoch_values[state.loader_name][metric_name] = metric_
+                loader_values[metric_name] = metric_
 
         for prefix in self.list_args:
+            # averages computed metrics
             mean_value = float(np.mean(prec_recall_f1score[prefix]))
             metric_name = f"{prefix}/_mean"
-            state.metrics.epoch_values[state.loader_name][metric_name] = mean_value
+            loader_values[metric_name] = mean_value
 
         self._reset_stats()
-
 
 __all__ = ["PrecisionRecallF1ScoreCallback"]
