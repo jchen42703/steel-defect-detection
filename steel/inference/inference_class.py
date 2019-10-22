099eae70e92c15722939bf927cd887d72a5a4472
jchen42703 <jchen42703@gmail.com>
2019-10-21 20:46:16 -0400

    Added the ability to ensemble (model and checkpoint_path args were changed to plural forms)

--- steel/inference/inference_class.py
+++ steel/inference/inference_class.py
@@ -12,26 +12,19 @@ from steel.inference.utils import mask2rle, post_process, load_weights_infer, \
                                   tta_flips_fn, apply_nonlin
 
 class Inference(object):
-    def __init__(self, checkpoint_path, test_loader, test_dataset, model=None,
+    def __init__(self, checkpoint_paths, test_loader, test_dataset, models=None,
                  mode="segmentation", tta_flips=None, sharpen_t=0.5):
         """
         Attributes:
-            checkpoint_path (str): Path to a checkpoint
+            checkpoint_paths (List[str]): Path to a checkpoint
             test_loader (torch.utils.data.DataLoader): Test loader
-            model (None or nn.Module): Only provide if your model weights are not traceable through torch.jit
+            models (List[None or nn.Module]): Only provide if your model weights are not traceable through torch.jit
             mode (str): either "segmentation" or "classification". Defaults to "segmentation"
             tta_flips (list-like): consisting one of or all of ["lr_flip", "ud_flip", "lrud_flip"].
                 Defaults to None.
             sharpen_t (float): Parameter to sharpen with
         """
-        try:
-            self.model = load(checkpoint_path).cuda()
-            print(f"Traced model from {checkpoint_path}")
-        except:
-            self.model = load_weights_infer(checkpoint_path, model).cuda()
-            print(f"Loaded model from {checkpoint_path}")
-        self.model.cuda()
-        self.model.eval()
+        self.load_checkpoints(checkpoint_paths, models)
 
         self.mode = mode
         self.loader = test_loader
@@ -46,6 +39,30 @@ class Inference(object):
             self.tta_fn = partial(tta_flips_fn, model=self.model, mode=mode,
                                   flips=tta_flips, non_lin="sigmoid",
                                   sharpen_t=sharpen_t)
+
+    def load_checkpoints(self, checkpoint_paths, models):
+        """
+        Either loads a single checkpoint or loads an ensemble of checkpoints
+        from `checkpoint_paths`
+        """
+        # single model instances
+        if isinstance(checkpoint_paths, str):
+            try:
+                self.model = load(checkpoint_paths).cuda().eval()
+                print(f"Traced model from {checkpoint_paths}")
+            except:
+                self.model = load_weights_infer(checkpoint_paths, models).cuda().eval()
+                print(f"Loaded model from {checkpoint_paths}")
+        # ensembled models
+        elif len(checkpoint_paths) > 1:
+            try:
+                self.model = EnsembleModel([load(path).cuda().eval()
+                                            for path in checkpoint_paths])
+            except:
+                self.model = EnsembleModel([load_weights_infer(path, model).cuda().eval()
+                                            for (path, model) in
+                                            zip(checkpoint_paths, models)])
+            print(f"Loaded an ensemble from {checkpoint_paths}")
 
     def create_sub(self, sub):
         """
@@ -134,3 +151,19 @@ class Inference(object):
                 predict = list(map(process, predict.flatten().tolist()))
                 predictions = predictions + predict
         return predictions
+
+class EnsembleModel(object):
+    """
+    Callable class for ensembled model inference
+    """
+    def __init__(self, models):
+        self.models = models
+        assert len(self.models) > 1
+
+    def __call__(self, x):
+        res = []
+        with torch.no_grad():
+            for m in self.models:
+                res.append(m(x))
+        res = torch.stack(res)
+        return torch.mean(res, dim=0)
